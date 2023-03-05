d5e20f3d46d35b9c41c96c20c51bd86ebd2ad7b6
Ryan E. Wilson <ryanwilson2005@gmail.com>
2023-03-05 06:04:27 -0500

    add psDisplay variable

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@
 const p1button = document.querySelector("#p1Button");
 const p2button = document.querySelector("#p2Button");
 let p1score = 0;
+const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector('#p2Display');
+
 
 p1button.addEventListener('click', function(){
     p1score += 1;
