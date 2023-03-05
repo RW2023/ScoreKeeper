7b058c5446cf1d59b5cac0cd1302cc82952863bf
Ryan E. Wilson <ryanwilson2005@gmail.com>
2023-03-05 06:28:55 -0500

    prevent scores from passing winning score

--- src/app.js
+++ src/app.js
@@ -5,15 +5,16 @@ let p1score = 0;
 let p2score = 0;
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector('#p2Display');
-
+let winningScore= 5;
 
 p1button.addEventListener('click', function(){
+    if(p1score !== winningScore)
     p1score += 1;
     p1Display.textContent = p1score;
 })
 
 p2button.addEventListener("click", function () {
-  p2score += 1;
+    if (p2score !== winningScore) p2score += 1;
   p2Display.textContent = p2score;
 });
 
