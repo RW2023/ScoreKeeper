9835bef22fb56e05235c5815be84cfb978777be6
Ryan E. Wilson <ryanwilson2005@gmail.com>
2023-03-05 06:31:57 -0500

    define "game over" variable

--- src/app.js
+++ src/app.js
@@ -4,17 +4,16 @@ const p2button = document.querySelector("#p2Button");
 let p1score = 0;
 let p2score = 0;
 const p1Display = document.querySelector("#p1Display");
-const p2Display = document.querySelector('#p2Display');
-let winningScore= 5;
+const p2Display = document.querySelector("#p2Display");
+let winningScore = 5;
+let isGameOver = false;
 
-p1button.addEventListener('click', function(){
-    if(p1score !== winningScore)
-    p1score += 1;
-    p1Display.textContent = p1score;
-})
-
-p2button.addEventListener("click", function () {
-    if (p2score !== winningScore) p2score += 1;
-  p2Display.textContent = p2score;
+p1button.addEventListener("click", function () {
+  if (p1score !== winningScore) p1score += 1;
+  p1Display.textContent = p1score;
 });
 
+p2button.addEventListener("click", function () {
+  if (p2score !== winningScore) p2score += 1;
+  p2Display.textContent = p2score;
+});
