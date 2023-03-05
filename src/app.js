bd34e3112fee540bb45122be9810bae11367a991
Ryan E. Wilson <ryanwilson2005@gmail.com>
2023-03-05 06:01:04 -0500

    Define Variables const p1button, const p2button, let p1score, const p2Display. Define p1button.addEventListener event

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// define variables
+const p1button = document.querySelector("#p1Button");
+const p2button = document.querySelector("#p2Button");
+let p1score = 0;
+const p2Display = document.querySelector('#p2Display');
+
+p1button.addEventListener('click', function(){
+    p1score += 1;
+})
+
