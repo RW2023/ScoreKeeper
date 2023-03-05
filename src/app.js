e0904c19a6a44b1306a8833561f94da7dd923c86
Ryan E. Wilson <ryanwilson2005@gmail.com>
2023-03-05 06:18:17 -0500

    add code to increment player 2 score

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 const p1button = document.querySelector("#p1Button");
 const p2button = document.querySelector("#p2Button");
 let p1score = 0;
+let p2score = 0;
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector('#p2Display');
 
@@ -10,4 +11,9 @@ p1button.addEventListener('click', function(){
     p1score += 1;
     p1Display.textContent = p1score;
 })
+
+p2button.addEventListener("click", function () {
+  p2score += 1;
+  p2Display.textContent = p2score;
+});
 
