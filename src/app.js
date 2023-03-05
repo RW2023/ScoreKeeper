ca233622d2c8997e8f724033be3c379e5b529ab5
Ryan E. Wilson <ryanwilson2005@gmail.com>
2023-03-05 06:43:57 -0500

    start game over logic

--- src/app.js
+++ src/app.js
@@ -6,14 +6,21 @@ let p2score = 0;
 const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
 let winningScore = 5;
-let isGameOver = false;
+let gameOver = false;
 
 p1button.addEventListener("click", function () {
-  if (p1score !== winningScore) p1score += 1;
+    if (!gameOver)
+  p1score += 1;
+  if (p1score === winningScore){
+    gameOver = true;
+}
   p1Display.textContent = p1score;
 });
 
 p2button.addEventListener("click", function () {
-  if (p2score !== winningScore) p2score += 1;
-  p2Display.textContent = p2score;
+   if (!gameOver) p2score += 1;
+   if (p2score === winningScore) {
+     gameOver = true;
+   }
+   p1Display.textContent = p2score;
 });
